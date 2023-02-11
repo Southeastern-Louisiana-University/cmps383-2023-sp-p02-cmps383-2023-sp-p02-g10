b84fa12f8e64f6954dde6ab1f86417568ae4f243
cyponos <98052408+cyponos@users.noreply.github.com>
2023-02-11 13:58:31 -0600

    Create Test.cs
    Just to make sure I don't mess up anything for yall

---
+++ SP23.P02.Tests.Web/Dtos/Test.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SP23.P02.Tests.Web.Dtos
+{
+    internal class Test
+    {
+        public string? UserName { get; set; }
+        public string? Password { get; set; }
+        public string[]? Roles { get; set; }
+    }
+}
